e9f1ece596ac3d0d3141e198f0268bcdb49ac82c
Troy Dorman <troydorman@gmail.com>
2021-08-16 07:28:15 -0600

    integrated handlebars view engine and other middleware

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
 const keys = require("./config/keys");
 const stripe = require("stripe")(keys.stripeSecretKey);
 
 // Create our express application
 const app = express();
 
+// handlebars middleware
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// express json middleware
+app.use(express.json({ extended: false }));
+
 const port = process.env.PORT || 5000;
+
+// create index get route
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
-
