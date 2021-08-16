26b4f2ade4fc6fcb98699b0c9b99bfeecb5bd201
Troy Dorman <troydorman@gmail.com>
2021-08-16 07:24:52 -0600

    started up basic express server on port 5000

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ const express = require("express");
 const keys = require("./config/keys");
 const stripe = require("stripe")(keys.stripeSecretKey);
 
-console.log(keys.stripeSecretKey);
+// Create our express application
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
